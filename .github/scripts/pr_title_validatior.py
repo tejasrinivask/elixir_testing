4a85547a68e4dec71690b2c0b384bdf45497936d
tejasrinivask <tejasrinivas@amagi.com>
2024-04-10 14:34:36 +0530

    Update pr validation to check the body for jira

--- .github/scripts/pr_title_validatior.py
+++ .github/scripts/pr_title_validatior.py
@@ -1,10 +1,25 @@
-import os
 import json
+import os
+import re
 
 PR_TITLE_PREFIX = "PR Merge"
 
 def validate_pr_body(body):
-    print(body)
+    jira_list = []
+    p = re.compile(r"^\s*\|\s*Jira ID\s*\|\s*")
+    for each_line in body.splitlines():
+        # print(each_line)
+        m = p.match(each_line)
+        if m:
+            data = each_line[m.end():].rstrip(" |")
+            if data:
+                tmp_list = [x.strip() for x in data.split(",")]
+                jira_list.extend(tmp_list)
+    if not jira_list:
+        print("exiting")
+        exit(1)
+    print(jira_list)
+    return
 
 def validate_pr_title(title):
     if PR_TITLE_PREFIX in title:
@@ -15,22 +30,41 @@ def validate_pr_title(title):
 
 def main():
     # Load pull request event payload
-    event_path = os.getenv('GITHUB_EVENT_PATH')
-    with open(event_path, 'r') as f:
-        try:
-            event_payload = json.load(f)
-        except Exception as e:
-            print("Error loading event payload, {e}")
-            exit(1)
+    # event_path = os.getenv('GITHUB_EVENT_PATH')
+    # with open(event_path, 'r') as f:
+    #     try:
+    #         event_payload = json.load(f)
+    #     except Exception as e:
+    #         print("Error loading event payload, {e}")
+    #         exit(1)
 
-    if not event_payload or 'pull_request' not in event_payload or not event_payload['pull_request']:
-        print(f"Invalid event payload : {event_payload}")
-        exit(1)
+    # if not event_payload or 'pull_request' not in event_payload or not event_payload['pull_request']:
+    #     print(f"Invalid event payload : {event_payload}")
+    #     exit(1)
 
-    title = event_payload['pull_request']['title']
-    validate_pr_title(title)
+    # # title = event_payload['pull_request']['title']
+    # # validate_pr_title(title)
 
-    body = event_payload['pull_request']['body']
+    # body = event_payload['pull_request']['body']
+    body = """
+| Jira ID | |
+| :--- | :--- |
+| Changed Component Name, In case code change is needed | |
+| What are the steps to reproduce this issue? | |
+| Are there any features likely to be impacted? If so, what are the features and what is impact? | |
+| How can this change be tested? Is there any additional tests to be done other than bug fix validation? | |
+| Should this fix be considered for LTS Release? | Yes / No |
+| Is the fix for this issue available in any of the latest CP releases? | |
+
+| Jira ID ||
+| :--- | :--- |
+| Changed Component Name, In case code change is needed | |
+| What are the steps to reproduce this issue? | |
+| Are there any features likely to be impacted? If so, what are the features and what is impact? | |
+| How can this change be tested? Is there any additional tests to be done other than bug fix validation? | |
+| Should this fix be considered for LTS Release? | Yes / No |
+| Is the fix for this issue available in any of the latest CP releases? | |
+    """
     validate_pr_body(body)
 
 
