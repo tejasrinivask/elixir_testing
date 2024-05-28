67f62bc9c030de04b6bf0e71abc0b88ce4ae76eb
tejasrinivask <tejasrinivas@amagi.com>
2024-05-28 13:14:25 +0530

    fix target path

--- .github/scripts/deploy.py
+++ .github/scripts/deploy.py
@@ -14,7 +14,7 @@ def main():
     """
     branch_pattern = sys.argv[1]
     with open(
-        "pr_and_build_notes/scripts/build_notes_configs.json",
+        "target_repo/.github/scripts/build_notes_configs.json",
         mode="r",
         encoding="utf-8",
     ) as fh:
