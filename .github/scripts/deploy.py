79135fb7b394a8702ff8aefffce9c955c8ab1f2c
tejasrinivask <tejasrinivas@amagi.com>
2024-05-28 12:54:10 +0530

    update branch pattern usage

--- .github/scripts/deploy.py
+++ .github/scripts/deploy.py
@@ -3,6 +3,7 @@
 Update branch pattern for pull request validator workflow
 """
 import sys
+import json
 
 from ruamel.yaml import YAML
 
@@ -12,23 +13,28 @@ def main():
     update branch pattern for pr_body_validator workflow file
     """
     branch_pattern = sys.argv[1]
-    yaml = YAML()
     with open(
-        "target_repo/.github/workflows/pr_body_validator.yaml",
-        mode="r",
-        encoding="utf-8",
+        ".github/scripts/build_notes_configs.json", mode="r", encoding="utf-8"
     ) as fh:
-        data = yaml.load(fh)
-    cmd = [i.strip() for i in data["jobs"]["BlockPR"]["steps"][1]["run"].split()[:-1]]
-    for pat in branch_pattern.split():
-        cmd.append(f"{pat}")
-    data["jobs"]["BlockPR"]["steps"][1]["run"] = " ".join(cmd)
-    with open(
-        "target_repo/.github/workflows/pr_body_validator.yaml",
-        mode="w",
-        encoding="utf-8",
-    ) as fh:
-        yaml.dump(data, fh)
+        data = json.load(fh)
+    data["branch_pattern"] = branch_pattern
+    # yaml = YAML()
+    # with open(
+    #     "target_repo/.github/workflows/pr_body_validator.yaml",
+    #     mode="r",
+    #     encoding="utf-8",
+    # ) as fh:
+    #     data = yaml.load(fh)
+    # cmd = [i.strip() for i in data["jobs"]["BlockPR"]["steps"][1]["run"].split()[:3]]
+    # for pat in branch_pattern.split():
+    #     cmd.append(f"{pat}")
+    # data["jobs"]["BlockPR"]["steps"][1]["run"] = " ".join(cmd)
+    # with open(
+    #     "target_repo/.github/workflows/pr_body_validator.yaml",
+    #     mode="w",
+    #     encoding="utf-8",
+    # ) as fh:
+    #     yaml.dump(data, fh)
 
 
 if __name__ == "__main__":
